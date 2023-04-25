439a2bbfa4737a58133321001158c85aabab912e
Melody <127463451+paper-girl@users.noreply.github.com>
2023-04-25 16:02:32 -0700

    Completed lab 5!! :)

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,45 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Ping_Pong 
+// An implementation of the game 
+// index.js - the purpose of this psuedocode is to draft out the steps of playing a game of ping pong 
+// Author: Melody Caudill
+// Date: 24 April 2023
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Initialize player 1 score to 0  
+// Initialize player 2 score to 0  
+// Initialize ball position x axis (ball_x) to 0  
+// Initialize ball position y axis (ball_y) to 0 
+// Initialize derivative with respect to x of ball (ball_dx) to 1 
+// Initialize derivative with respect to y of ball (ball_dy) to 1 
+// Set winning store to 10 
+// Print score of each player starting at 0 
+// Loop while true: 
+// If key “w” is pressed, player 1 score is subtracted by 1  point
+	// Update player 1 score
+	// Print player 1 score
+// Else-if “s” is pressed, player 1 score is added 1 point
+	// Update Player 1 score
+	// Print player 1 score
+// If “up arrow” is pressed, player 2 score is subtracted by 1 point 
+// Update player 2 score
+	// Print player 2 score
+// Else-if  “down arrow” is pressed, player 2 score is added 1 point 
+// Update player 2 score
+	// Print player 2 score
+ // Update the the ball_x variable by adding the ball_dx variable to it 
+//  Update the the ball_y variable by adding the ball_dy variable to it 
+	// if ball_y is equal to 0
+// subtract 1 from ball_dy 
+// if ball_x is equal to 0
+	// Player 2 score is added 1 point 
+	// Reset ball position
+// else if ball_x is equal to the screen width - 1
+	// Player 1 score is added 1 point 
+	// Reset ball position
+// if ball_x is equal to 1 and player 1 score is less than or equal to ball_y which is less than player 1 score plus the paddle height 
+// Multiply the current value of ball_dx by -1 which reverses the direction of the horizontal motion    
+// Else-if ball_x is equal to screen width - 2 and player 2 score is less than or equal to ball_y which is less than player 2 score plus the paddle height 
+// multiply the current value of ball_dx by -1 which reverses the direction of the horizontal motion    
+	// If player 1 score is equal to 10, 
+// Print “player 1 is the winner”
+// If player 2 score is equal to 10, 
+// Print “player 2 is the winner”
---
+++ lab5/js/lab5.js
@@ -0,0 +1,39 @@
+// index.js - Creating JavaScript of a car 
+// Author: Melody Caudill and Hanna Nunnelley
+// Date: 25 April 2023 
+
+// Constants
+
+// Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+    // some code here
+    // return results;
+  }
+  
+  function main() {
+    console.log("Main function started.");
+    // the code that makes everything happen
+  }
+  
+  // let's get this party started
+  main();
+  
+// Define Variables 
+carMake = "Honda"; 
+carModel = "Civic";
+carColor = "Gray";
+carYear = 2009;
+
+// calculate 
+age =  2023  - carYear; 
+
+// output 
+document.writeln("Make: " + carMake + "<br>");
+document.writeln("Model: " + carModel + "<br>");
+document.writeln("Color: " + carColor + "<br>");
+document.writeln("Year: " + carYear + "<br>");
+document.writeln("Age: " + carAge + " years<br>");
+
+
