b7dd11a7250b25cd45305d40d7862d5decd6f1d8
Melody <127463451+paper-girl@users.noreply.github.com>
2023-04-29 14:20:44 -0700

    completed lab 6 and new css changes!

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,29 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6: Arrays and Objects
+// Author: Melody Caudill
+// Date: 28 April 2023 
 
-// Constants
+//Define Variables
+//Array
+myTransport = ["shuttle", "walk"];
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+//create object for my main ride 
+//Object
+var myMainRide = {
+  make: "Honda",
+  model: "Civic",
+  color: "Gray",
+  year: "2009",
+  age: function() {
+      return 2023 - this.year;
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+//output 
+document.writeln("Getting around: " + myTransport + "<br>");
+document.writeln("This is how I get around! ",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
+    
 
-// let's get this party started
-main();
+
+
+
