a66f20a2f6ed3db8455536a8fc7b699a571c8ba0
Melody <127463451+paper-girl@users.noreply.github.com>
2023-05-08 11:32:19 -0700

    finished lab 8!

---
+++ lab8/js/lab8.js
@@ -0,0 +1,24 @@
+//index.js: Anon Functions and Callbacks 
+//Author: Melody and Hanna
+//Date: 8 May 2023 
+
+function melodyFunction (x) {
+    var results = x + 1 
+    return results 
+}
+
+//test fucntion 
+console.log(melodyFunction(7));
+console.log(melodyFunction(64));
+
+array = [4, 16, 24, 40]
+console.log("My array", array);
+
+var result = array.map(melodyFunction); 
+console.log("Testing  the function", result);
+
+array.map(function(x) {
+    var results = x * 2
+    return results; 
+})
+    
